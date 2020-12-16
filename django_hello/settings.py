5fd3c3c48738734b7f6c030783030cb5b9a7cadf
tango <tangofun@live.jp>
2020-12-16 11:45:13 -0300

    V1.0.0

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'django_hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {    
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }    
 }
-"""
+
 
 
 # Password validation
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
-
+from hello.models import LogMessage
 from . import views
+from django.contrib import admin
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="hello/home.html",
+)
 
 urlpatterns = [    
-    path('', views.hello, name='hello'),
+    # path('', views.hello, name='hello'),
+    # path("hello/<name>", views.hello_there, name="hello_there"),
+    path("", home_list_view, name="home"),
+    # path("home/", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
+    path("admin/", admin.site.urls),
 ]
+
